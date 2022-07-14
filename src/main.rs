225605c92f02f8629c6d78287462110901ba5919
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-07-14 23:33:07 +0430

    modules in separate files

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,11 @@
-use http::method::Method;
-use http::request::Request;
+use http::Method;
+use http::Request;
 use server::Server;
 
-fn main() {
-    let get = Method::GET;
-    let post = Method::POST;
-    let delete = Method::DELETE;
-    let put = Method::PUT;
+mod http;
+mod server;
 
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run()
-}
-
-pub mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Server { addr }
-        }
-
-        pub fn run(self) {
-            println!("Server is listening on {}", self.addr)
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-
-    pub mod method {
-        pub enum Method {
-            GET,
-            POST,
-            DELETE,
-            PUT,
-            PATCH,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-        }
-    }
 }
---
+++ src/server.rs
@@ -0,0 +1,13 @@
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Server { addr }
+    }
+
+    pub fn run(self) {
+        println!("Server is listening on {}", self.addr)
+    }
+}
